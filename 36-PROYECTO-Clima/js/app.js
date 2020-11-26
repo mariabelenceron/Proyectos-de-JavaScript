d88a811ca2da3a269349e5e3aa9b98e92a2fbc5b
Maria Belen Ceron <mabelenceron@gmail.com>
2020-11-26 15:55:31 -0500

    Proyecto sobre el clima consumiendo datos de una API

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,142 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', ()=>{
+    formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    //Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
+
+    //Consultar API
+    consultarAPI(ciudad, pais);
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta){
+        //Crear alerta
+        const alerta = document.createElement('div');
+    
+        alerta.classList.add('bg-red-100', 'border-red-400','text-red-700','px-4','py-3','rounded','max-w-md','mx-auto','mt-6','text-center');
+    
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+    
+        container.appendChild(alerta);
+        
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+function consultarAPI(ciudad, pais) {
+    
+    const appId = 'b4608c6b82e43a049d30d89a489bf557';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    Spinner(); // Muestra un spinner de carga
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            limpiarHTML(); //Limpia el HTML
+            console.log(datos);
+
+            if(datos.cod === '404'){
+                mostrarError('Ciudad no encontrada');
+                return;
+            }
+
+            //Imprime la respuesta en el HTML
+            mostrarClima(datos);
+        })
+}
+
+function mostrarClima(datos) {
+    const {weather: [{icon}],name, main: { temp, temp_max, temp_min } } = datos;
+
+    const divInfoPrincipalTemp = document.createElement('div');
+    
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+    
+    const nombreCiudad = document.createElement('p');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl');
+    
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold','text-6xl');
+
+    const urlIcono = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const iconoTemp = document.createElement('img');
+    iconoTemp.setAttribute('src',urlIcono);
+
+    divInfoPrincipalTemp.classList.add('divInfoPrincipalTemp');
+    divInfoPrincipalTemp.appendChild(iconoTemp);
+    divInfoPrincipalTemp.appendChild(actual);
+
+    const tempMaxima = document.createElement('p');
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;
+    tempMaxima.classList.add('text-xl');
+    const tempMinima = document.createElement('p');
+    tempMinima.innerHTML = `Min: ${min} &#8451;`;
+    tempMinima.classList.add('text-xl');
+    
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(divInfoPrincipalTemp);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+function limpiarHTML() {
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function Spinner() {
+    limpiarHTML();
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
+}
